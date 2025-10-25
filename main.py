567a3925cb61c47ccb746b93f9d59a435b7b1e38
Marc S. <168722619+CryptixByte@users.noreply.github.com>
2025-10-24 20:28:48 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,72 @@
+from twitchio.ext import commands
+import random
+
+
+
+
+#=================
+#UNCOMMENT ON RELEASE
+#=================
+
+bot = commands.Bot(
+    token='oauth:[PLACEHOLDER]',
+    client_id='[PLACEHOLDER]',
+    nick='[PLACEHOLDER]',
+    prefix='!',
+    initial_channels=['[PLACEHOLDER]']
+)
+
+
+#Basic Twitch Commands with Song Requests System
+@bot.event
+async def event_message(message):
+    if message.author.name.lower() == bot.nick.lower():
+        return
+    await bot.handle_commands(message)
+
+@bot.command(name='socials')
+async def socials(ctx):
+    await ctx.send(f'YouTube: https://www.youtube.com/@EclipsedSpirit | Discord: discord.gg/m88z4HVE')
+
+#Song Request System
+#========================================================
+#Delete this section if you do not want song requests
+#This section was designed for OperationCaboose on TwitchTV
+#========================================================
+
+@bot.command(name='songrequest')
+async def song(ctx):
+    await ctx.send(f'Placing song in the queue!')
+
+@bot.command(name='song')
+async def song(ctx):
+    await ctx.send(f'current song: {ctx.message}')
+
+
+#Yu-Gi-Oh Streamer Commands
+#========================================================
+#Delete this section if you do not want Yu-Gi-Oh Commands
+#This section was designed for Averyxstream on TwitchTV
+#========================================================
+
+#Generates a random Bamu (Bullshit avery made up) deck
+@bot.command(name='bamu')
+async def bamu(ctx):
+    await ctx.send(f'test')
+
+#Generates a random Edison deck
+@bot.command(name='edison')
+async def edison(ctx):
+    edison_list = [
+        "Earthbound", "Cyberdark", "Neos", "Insect", "Ninja", "Ojama", "Reptile", "Roids", "Tryhard DAD"
+    ]
+    await ctx.send(f'The chosen edison deck is: {random.choice(edison_list)}')
+
+#Generates a random Deck
+@bot.command(name='randomdeck')
+async def randomdeck(ctx):
+    await ctx.send(f'test')
+
+if __name__ == "__main__":
+    print("🚀 Launching bot . . .")
+    bot.run()
---
+++ readme.md
@@ -0,0 +1,20 @@
+# EMusicBot v0.1
+
+## Overview
+A basic twitch chat bot that utilizes the libraries: twitchio v2.8.2, and [PLACEHOLDER]
+
+### Usage
+- 
+
+### Features
+- Basic AutoMod features (Future)
+- Several Basic Bot Commands 
+- Song Requests (Future)
+
+#### Development Notes
+
+- Used 'curl -H "Client-ID: [Client ID]" -H "Authorization: Bearer [OAuth Token]" "https://api.twitch.tv/helix/users?login=emusicbot"' to get my Bot ID
+
+- https://id.twitch.tv/oauth2/authorize?client_id=zpdbffu9rc3ps3ocrt5pm3lppzxq9i&redirect_uri=http://localhost&response_type=token&scope=chat:read+chat:edit
+
+- Used 'curl -H "Client-ID: dkoikn7irbd5l2zlwl1x6xzhxvmfoi" -H "Authorization: Bearer zpdbffu9rc3ps3ocrt5pm3lppzxq9i" "https://api.twitch.tv/helix/users?login=emusicbot"'
